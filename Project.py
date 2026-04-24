aedb99917b8fd243aa3fb9a87342fe4e88877901
Isaac <23imccartney@wakatipu.school.nz>
2026-04-24 14:29:55 +1200

    Fix error with print menu, had a "}" in the wrong place

--- Project.py
+++ Project.py
@@ -17,55 +17,50 @@ def main():
 def print_menu():
     print("Sushis:")
     for item, info in menu.items():
-        print(item, info)
+        if info["food_type"] == "sushi":
+            print(item)
 
 
-def order():
-    pass
-
-    
 menu = {
-"Salmon Nigiri" : {"price" : 3.50, "type" : "sushi"},
+"Salmon Nigiri" : {"price" : 3.50, "food_type" : "sushi"},
 
-"Tuna Sashimi" : {"price" : 4.50, "type" : "sushi"},
+"Tuna Sashimi" : {"price" : 4.50, "food_type" : "sushi"},
 
-"California Roll" : {"price" : 6.00, "type" : "sushi"},
+"California Roll" : {"price" : 6.00, "food_type" : "sushi"},
 
-"Spicy Tuna Roll" : {"price" : 6.50, "type" : "sushi"},
+"Spicy Tuna Roll" : {"price" : 6.50, "food_type" : "sushi"},
 
-"Dragon Roll" : {"price" : 12.00, "type" : "sushi"},
+"Dragon Roll" : {"price" : 12.00, "food_type" : "sushi"},
 
-"Cucumber Maki" : {"price" : 3.00, "type" : "sushi"},
+"Cucumber Maki" : {"price" : 3.00, "food_type" : "sushi"},
 
-"Teriyaki Chicken Bento" : {"price" : 14.50, "type" : "box"},
+"Teriyaki Chicken Bento" : {"price" : 14.50, "food_type" : "box"},
 
-"Beef Yakiniku Bento" : {"price" : 16.00, "type" : "box"},
+"Beef Yakiniku Bento" : {"price" : 16.00, "food_type" : "box"},
 
-"Salmon Teriyaki Bento" : {"price" : 15.50}, "type" : "box",
+"Salmon Teriyaki Bento" : {"price" : 15.50, "food_type" : "box"},
 
-"Vegetable Tempura Bento" : {"price" : 13.00, "type" : "box"},
+"Vegetable Tempura Bento" : {"price" : 13.00, "food_type" : "box"},
 
-"Chicken Yakisoba" : {"price" : 12.50, "type" : "noodles"},
+"Chicken Yakisoba" : {"price" : 12.50, "food_type" : "noodles"},
 
-"Shrimp Tempura Udon" : {"price" : 13.50, "type" : "noodles"},
+"Shrimp Tempura Udon" : {"price" : 13.50, "food_type" : "noodles"},
 
-"Tonkotsu Ramen" : {"price" : 14.00, "type" : "noodles"},
+"Tonkotsu Ramen" : {"price" : 14.00, "food_type" : "noodles"},
 
-"Vegetable Yaki Udon" : {"price" : 11.50, "type" : "noodles"},
+"Vegetable Yaki Udon" : {"price" : 11.50, "food_type" : "noodles"},
 
-"Miso soup" : {"price" : 2.50, "type" : "drink"},
+"Miso soup" : {"price" : 2.50, "food_type" : "drink"},
 
-"Coca-cola" : {"price" : 3.00, "type" : "drink"},
+"Coca-cola" : {"price" : 3.00, "food_type" : "drink"},
 
-"Sprite" : {"price" : 3.00, "type" : "drink"},
+"Sprite" : {"price" : 3.00, "food_type" : "drink"},
 
-"Apple juice" : {"price" : 2.50, "type" : "drink"},
+"Apple juice" : {"price" : 2.50, "food_type" : "drink"},
 }
 
 os.system('cls')
 
-
 print_menu()
-
 
 print()
